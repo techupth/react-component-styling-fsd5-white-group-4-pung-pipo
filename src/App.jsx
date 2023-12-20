064feab5d7d859e1f8dbc1fcd61f5b5bbdb3cc42
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-20 18:02:47 +0700

    feat(exercise): add completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
+import { Button } from "./components/Button";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+        <Button type="Primary" text="Button" />
+        <Button type="Secondary" text="Button" />
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+        <Alert type="error" text="This is error alert box" />
+        <Alert type="alert" text="This is warning alert box" />
+        <Alert type="info" text="This is info alert box" />
+        <Alert type="success" text="This is success alert box" />
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,36 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+export function Button(props) {
+  if (props.type === "Primary") {
+    return (
+      <button
+        css={css`
+          background-color: blue;
+          color: white;
+          width: 200px;
+          height: 50px;
+          text: Button;
+          margin-right: 10px;
+        `}
+      >
+        {props.text}
+      </button>
+    );
+  } else if (props.type === "Secondary") {
+    return (
+      <button
+        css={css`
+          background-color: #03a9f4;
+          color: white;
+          width: 200px;
+          height: 50px;
+          text: Button;
+        `}
+      >
+        {props.text}
+      </button>
+    );
+  }
+}
